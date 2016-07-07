386f146c010a14bdcdfcba0765bd96206a8fb7d5
Arthur Petukhovsky <petuhovskiy@yandex.ru>
2016-07-07 16:36:13 +0300

    temp test

---
+++ src/test/java/TempTest.java
@@ -0,0 +1,25 @@
+import com.petukhovsky.jvaluer.commons.util.temp.TempDirectory;
+import com.petukhovsky.jvaluer.commons.util.temp.TempStorage;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+/**
+ * Created by Arthur Petukhovsky on 7/7/2016.
+ */
+public class TempTest {
+    @Test
+    public void testDirectory() throws IOException {
+        Path dir = Files.createTempDirectory("");
+        TempStorage storage = new TempStorage(dir);
+        try (TempDirectory temp = storage.createTempDirectory()) {
+            Path file = temp.resolve("file.ex");
+            ByteArrayInputStream bais = new ByteArrayInputStream("test test string".getBytes());
+            Files.copy(bais, file, StandardCopyOption.REPLACE_EXISTING);
+        }
+    }
+}
