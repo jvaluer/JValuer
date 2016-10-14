a75078acb070c205e83a7be22b42dae54a172c5e
Arthur Petukhovsky <petuhovskiy@yandex.ru>
2016-10-14 18:43:24 +0300

    invoker builtin

--- src/main/java/com/petukhovsky/jvaluer/commons/builtin/JValuerBuiltin.java
+++ src/main/java/com/petukhovsky/jvaluer/commons/builtin/JValuerBuiltin.java
@@ -2,6 +2,7 @@ package com.petukhovsky.jvaluer.commons.builtin;
 
 import com.petukhovsky.jvaluer.commons.checker.Checker;
 import com.petukhovsky.jvaluer.commons.gen.Generator;
+import com.petukhovsky.jvaluer.commons.invoker.Invoker;
 
 /**
  * Created by arthur on 14.10.16.
@@ -9,4 +10,5 @@ import com.petukhovsky.jvaluer.commons.gen.Generator;
 public interface JValuerBuiltin {
     Checker checker(String id);
     Generator generator(String id);
+    Invoker invoker(String id);
 }
