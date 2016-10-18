9d602aa63f4568184e10a80c3c6d4af4d4a8c087
Arthur Petukhovsky <petuhovskiy@yandex.ru>
2016-10-18 21:16:26 +0300

    interface generator

--- src/main/java/com/petukhovsky/jvaluer/commons/gen/Generator.java
+++ src/main/java/com/petukhovsky/jvaluer/commons/gen/Generator.java
@@ -7,14 +7,6 @@ import com.petukhovsky.jvaluer.commons.run.RunLimits;
 /**
  * Created by arthur on 14.10.16.
  */
-public abstract class Generator {
-    public GeneratedData generate(TestData in, RunLimits limits, String... args) {
-        return generate(RunLimits.ofTime(10000L), args);
-    }
-
-    public GeneratedData generate(RunLimits limits, String... args) {
-        return generate(args);
-    }
-
-    public abstract GeneratedData generate(String... args);
+public interface Generator {
+    GeneratedData generate(TestData in, RunLimits limits, String... args);
 }
